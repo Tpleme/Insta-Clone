0d45ac8295e56d4864c0a9818dc092f528f44143
Leandro Melo <Tpleme@hotmail.com>
2021-05-23 12:04:02 +0100

    Added take picture and pick from gallery functionallity

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,31 +1,53 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
-    const [hasPermission, setHasPermission] = useState(null);
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
     const [camera, setCamera] = useState(null);
+    const [image, setImage] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
 
     useEffect(() => {
         (async () => {
-            const { status } = await Camera.requestPermissionsAsync();
-            setHasPermission(status === 'granted');
+            const cameraStatus = await Camera.requestPermissionsAsync();
+            setHasCameraPermission(cameraStatus.status === 'granted');
+
+            const galleryStatus = await ImagePicker.requestCameraRollPermissionsAsync();
+            setHasGalleryPermission(galleryStatus.status === 'granted');
+
         })();
     }, []);
 
     const takePicture = async () => {
         if (camera) {
             const data = await camera.takePictureAsync(null);
-            console.log(date.uri);
+            setImage(data.uri);
         }
     }
 
-    if (hasPermission === null) {
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
+    if (hasCameraPermission === null || hasGalleryPermission === null) {
         return <View></View>
     }
 
-    if (hasPermission === false) {
+    if (hasCameraPermission === false || hasGalleryPermission == null) {
         return <Text>No access to camera</Text>
     }
 
@@ -49,6 +71,8 @@ export default function App() {
                 }}>
             </Button>
             <Button title="Take picture" onPress={() => takePicture()}/>
+            <Button title="Pick Image From Gallery" onPress={() => pickImage()}/>
+            {image && <Image source={{uri: image}} style={{ felx: 1 }}/>}
         </View>
     );
 }
