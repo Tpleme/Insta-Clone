36d39059c573a5fbfca66d3e399c5b40fe06c558
Leandro Melo <Tpleme@hotmail.com>
2021-05-27 17:05:41 +0100

    Added feed that display all the followers posts

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,27 +1,28 @@
-import firebase from 'firebase';
-import { USER_STATE_CHANGE, USER_POST_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE } from '../constants/index';
+import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE, USERS_DATA_STATE_CHANGE,USERS_POSTS_STATE_CHANGE} from '../constants/index'
+import firebase from 'firebase'
+require('firebase/firestore')
 
 export function fetchUser() {
-    return ((dispacth) => {
+    return ((dispatch) => {
         firebase.firestore()
             .collection("users")
             .doc(firebase.auth().currentUser.uid)
             .get()
             .then((snapshot) => {
                 if (snapshot.exists) {
-                    dispacth({ type: USER_STATE_CHANGE, currentUser: snapshot.data() })
+                    dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data() })
                 }
                 else {
-                    console.log('does not exist');
+                    console.log('does not exist')
                 }
             })
     })
 }
 
 export function fetchUserPosts() {
-    return ((dispacth) => {
+    return ((dispatch) => {
         firebase.firestore()
-            .collection("post")
+            .collection("posts")
             .doc(firebase.auth().currentUser.uid)
             .collection("userPosts")
             .orderBy("creation", "asc")
@@ -30,15 +31,15 @@ export function fetchUserPosts() {
                 let posts = snapshot.docs.map(doc => {
                     const data = doc.data();
                     const id = doc.id;
-                    return { id, ...data };
+                    return { id, ...data }
                 })
-                dispacth({ type: USER_POST_STATE_CHANGE, posts })
+                dispatch({ type: USER_POSTS_STATE_CHANGE, posts })
             })
     })
 }
 
 export function fetchUserFollowing() {
-    return ((dispacth) => {
+    return ((dispatch) => {
         firebase.firestore()
             .collection("following")
             .doc(firebase.auth().currentUser.uid)
@@ -46,9 +47,59 @@ export function fetchUserFollowing() {
             .onSnapshot((snapshot) => {
                 let following = snapshot.docs.map(doc => {
                     const id = doc.id;
-                    return id;
+                    return id
                 })
-                dispacth({ type: USER_FOLLOWING_STATE_CHANGE, following })
+                dispatch({ type: USER_FOLLOWING_STATE_CHANGE, following });
+                for(let i = 0; i < following.length; i++){
+                    dispatch(fetchUsersData(following[i]));
+                }
+            })
+    })
+}
+
+export function fetchUsersData(uid) {
+    return ((dispatch, getState) => {
+        const found = getState().usersState.users.some(el => el.uid === uid);
+        if (!found) {
+            firebase.firestore()
+                .collection("users")
+                .doc(uid)
+                .get()
+                .then((snapshot) => {
+                    if (snapshot.exists) {
+                        let user = snapshot.data();
+                        user.uid = snapshot.id;
+
+                        dispatch({ type: USERS_DATA_STATE_CHANGE, user });
+                        dispatch(fetchUsersFollowingPosts(user.uid));
+                    }
+                    else {
+                        console.log('does not exist')
+                    }
+                })
+        }
+    })
+}
+
+export function fetchUsersFollowingPosts(uid) {
+    return ((dispatch, getState) => {
+        firebase.firestore()
+            .collection("post")
+            .doc(uid)
+            .collection("userPosts")
+            .orderBy("creation", "asc")
+            .get()
+            .then((snapshot) => {
+                const uid = snapshot.query.EP.path.segments[1];
+                const user = getState().usersState.users.find(el => el.uid === uid);
+
+                let posts = snapshot.docs.map(doc => {
+                    const data = doc.data();
+                    const id = doc.id;
+                    return { id, ...data, user }
+                })
+                dispatch({ type: USERS_POSTS_STATE_CHANGE, posts, uid })
+
             })
     })
 }
--- redux/reducers/user.js
+++ redux/reducers/user.js
@@ -1,24 +1,24 @@
-import { USER_STATE_CHANGE, USER_POST_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE } from "../constants"
+import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE } from "../constants"
 
 const initialState = {
     currentUser: null,
     posts: [],
-    following: []
+    following: [],
 }
 
 export const user = (state = initialState, action) => {
-
     switch (action.type) {
         case USER_STATE_CHANGE:
             return {
                 ...state,
                 currentUser: action.currentUser
             }
-        case USER_POST_STATE_CHANGE:
+        case USER_POSTS_STATE_CHANGE:
             return {
                 ...state,
                 posts: action.posts
             }
+
         case USER_FOLLOWING_STATE_CHANGE:
             return {
                 ...state,
